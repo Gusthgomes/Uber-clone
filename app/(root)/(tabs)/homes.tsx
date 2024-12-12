3c03979b059675225049dedfb7891d1686dd1dc7
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-11 21:24:05 -0300

    feat: create initial pages and yours layouts

--- app/(root)/(tabs)/homes.tsx
+++ app/(root)/(tabs)/homes.tsx
@@ -1,10 +1,11 @@
-import { Redirect } from "expo-router"
 import { Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 const Home = () => {
     return (
-        <Redirect href="/(auth)/welcome"/>
+        <SafeAreaView>
+            <Text className="text-red-500">Home</Text>
+        </SafeAreaView>
     )
 }
 

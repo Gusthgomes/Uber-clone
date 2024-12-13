6cccafbaedd2f51ee3b60c11cdb43ec5b26be7e1
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-13 20:30:34 -0300

    feat: home screen development completed

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -12,6 +12,8 @@ const OnBoarding = () => {
     const swiperRef = useRef<Swiper>(null);
 
     const [activenIdex, setActiveIndex] = useState(0);
+    const isLastSlide = activenIdex === onboarding.length - 1;
+
     return (
         <SafeAreaView className="flex h-full items-center justify-between bg-white">
             <TouchableOpacity className="w-full flex justify-end items-end p-5" onPress={ () => {
@@ -39,12 +41,22 @@ const OnBoarding = () => {
                             <View className="flex flex-row items-center justify-center w-full mt-10">
                                 <Text className="text-black text-3xl font-bold mx-10 text-center">{item.title}</Text>
                             </View>
-                            <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">{item.description}</Text>
+                            <Text className="text-sm font-JakartaSemiBold text-center text-[#858585] mx-10 mt-2 mb-20">{item.description}</Text>
                         </View>
                     ))}
                 </Swiper>
 
-                <CustomButton title="Next" className="w-11/12 mt-10"/>
+                <CustomButton 
+                    title={ isLastSlide ? "Vamos nessa!" : "Próximo"}
+                    className="w-11/12 mt-10"
+                    onPress={() => {
+                        if(isLastSlide) {
+                            router.replace('/(auth)/sign-up')
+                        } else {
+                            swiperRef.current?.scrollBy(1)
+                        }
+                    }}
+                />
         </SafeAreaView>
     )
 }

dba54b77e7b821a3f071d275c7b3ec650f9b975a
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-13 20:29:18 -0300

    feat: add text variant style

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -16,6 +16,21 @@ const getBgVariantStyle = (variant: ButtonProps['bgVariant']) => {
     }
 }
 
+const getTextVariantStyle = (variant: ButtonProps['textVariant']) => {
+    switch(variant) {
+        case 'primary':
+            return 'text-black';
+        case 'secondary':
+            return 'bg-gray-100';
+        case 'danger':
+            return 'bg-red-100'
+        case 'success':
+            return 'bg-green-100'
+        default:
+            return 'text-white';
+    }
+}
+
 const CustomButton = ({
     onPress,
     title,
@@ -27,10 +42,12 @@ const CustomButton = ({
     ...props
 }: ButtonProps) => {
     return(
-        <TouchableOpacity onPress={ onPress } className={`
-        w-full rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}>
+        <TouchableOpacity 
+            onPress={ onPress } 
+            className={`w-full rounded-md p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+            {...props }>
             {IconLeft && <IconLeft />}
-            <Text>{ title }</Text>
+            <Text className={` text-lg font-bold ${getTextVariantStyle(textVariant)}`}>{ title }</Text>
             {IconRight && <IconRight />}
         </TouchableOpacity>
     )

d747d938b597af0f40e0650aa8be50e0bff059ac
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-13 21:42:46 -0300

    feat: initial sign up page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,54 @@
-import { Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { ScrollView, View, Image, Text } from "react-native";
+import { images, icons } from "@/constants";
+import InputField from "@/components/InputField";
+
+import { useState } from "react";
 
 const SignUp = () => {
+
+    const [ form, setForm ] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+
     return (
-        <SafeAreaView>
-            <Text className="text-red-500">SignUp</Text>
-        </SafeAreaView>
+        <ScrollView className="flex-1 bg-white">
+            <View className='flex-1 bg-white'>
+                <View className="relative w-full h-[250px]">
+                    <Image source={ images.signUpCar} className="z-0 w-full h-[250px]"/>
+                </View> 
+                <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">Crie a sua conta</Text>
+            </View>
+
+            <View className="p-5">
+                <InputField
+                label="Name"
+                placeholder="Digite o seu nome"
+                icon={icons.person}
+                value={form.name}
+                onChangeText={(value) => setForm({ ...form, name: value })}
+                />
+
+            <InputField
+            label="Email"
+            placeholder="Digite o seu e-mail"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Digite a sua senha"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+            </View>
+        </ScrollView>
     )
 }
 
